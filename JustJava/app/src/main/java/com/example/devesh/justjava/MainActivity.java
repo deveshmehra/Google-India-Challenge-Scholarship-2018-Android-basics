2597753cda91785d9fa9cb7ed68b3eb32a89e2a8
deveshmehra <deveshmehra123@gmail.com>
2018-03-20 20:12:37 +0530

    new code lines

--- JustJava/app/src/main/java/com/example/devesh/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/devesh/justjava/MainActivity.java
@@ -32,7 +32,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String priceMessage="Free's";
+        String priceMessage = "Total:" + "$" + (quantity*5);
+        priceMessage = priceMessage + "\nThank you!";
         displayMessage(priceMessage);
 
 
